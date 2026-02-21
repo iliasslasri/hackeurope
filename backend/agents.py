9daf2eec339b313d5a1287ef83822d6b9b7d0e3b
Iliass Lasri <iliass.lasri@outlook.com>
2026-02-21 23:00:37 +0100

    add new calls in agents

--- backend/agents.py
+++ backend/agents.py
@@ -1,19 +1,24 @@
 
 from backend.schemas import PatientHistory
 from backend.llm_client import call_llm
-from backend.prompts import INFORMATION_EXTRACTOR_SYSTEM
-
+from backend.prompts import INFORMATION_EXTRACTOR_SYSTEM, QUESTION_GENIE_SYSTEM, QUESTION_GENIE_USER_TEMPLATE
+from backend.schemas import AuraUIPayload
 
 class AuraPipeline:
     def __init__(self):
         self.patient_history = PatientHistory()
 
     def run(self, full_transcript: str) -> AuraUIPayload:
-        json_object = call_llm(INFORMATION_EXTRACTOR_SYSTEM, full_transcript)
-        self.patient_history.update(json_object)
+        # first call to extract info from the conversation
+        info_json = call_llm(INFORMATION_EXTRACTOR_SYSTEM, full_transcript)
+        self.patient_history.update(info_json)
         
-        ai_analysis = AuraUIPayload(
+        # second call to generate the ai analysis
+        prompt = QUESTION_GENIE_SYSTEM + QUESTION_GENIE_USER_TEMPLATE.format(patient_history=self.patient_history)
+        diseases_question_json = call_llm(prompt, self.patient_history)
+        
+        ui_payload = AuraUIPayload(
             patient_history=self.patient_history,
-            ai_analysis=json_object
+            ai_analysis=diseases_question_json
         )     
-        return ai_analysis
+        return ui_payload
